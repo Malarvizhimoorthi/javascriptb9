447ea6114e4a946be71925f8a362da796c6185d2
Malarvizhimoorthi <161681640+Malarvizhimoorthi@users.noreply.github.com>
2024-05-25 15:26:07 +0530

    comit message

---
+++ ES6/Demo.js
@@ -0,0 +1,11 @@
+/* basic javascript */
+var username="gayathri";
+console.log(username)
+
+/*Es6*/
+let password="gayu@123";
+console.log(password)
+const element="javascript is one of the famous language ";
+console.log(element)
+element="hai";
+console.log(element)
